508b372224f6d555249ed89b47a99a9a9d2a8fc1
Rafael Santos <15151000+blackmagicbox@users.noreply.github.com>
2026-05-08 16:55:32 +0200

    FEAT Implement vector operations: subtraction, multiplication, and division for Vec3

--- src/main.rs
+++ src/main.rs
@@ -21,10 +21,61 @@ impl ops::Add<Vec3> for Vec3 {
     }
 }
 
+impl ops::Sub<Vec3> for Vec3 {
+    type Output = Vec3;
+    fn sub(self, other: Vec3) -> Vec3 {
+        let new_x = self.x - other.x;
+        let new_y = self.y - other.y;
+        let new_z = self.z - other.z;
+        Vec3 {
+            x: new_x,
+            y: new_y,
+            z: new_z,
+        }
+    }
+}
+
+impl ops::Mul<f64> for Vec3 {
+    type Output = Vec3;
+    fn mul(self, scalar: f64 ) -> Vec3 {
+        Vec3{
+            x: self.x * scalar,
+            y: self.y * scalar,
+            z: self.z * scalar
+        }
+    }
+}
+
+impl ops::Div<f64> for Vec3 {
+    type Output = Vec3;
+    fn div(self, scalar: f64) -> Vec3 {
+        Vec3{
+            x: self.x / scalar,
+            y: self.y / scalar,
+            z: self.z / scalar
+        }
+    }
+}
+
+impl ops::Mul<Vec3> for Vec3 {
+    type Output = Vec3;
+    fn mul(self, other: Vec3) -> Vec3 {
+        Vec3{
+            x: self.x * other.x,
+            y: self.y * other.y,
+            z: self.z * other.z
+        }
+    }
+}
+
 fn main() {
 
     let pos1= Vec3{x:1.0, y:0.0, z:1.0};
-    let post2 = Vec3{x:2.0, y:3.0, z:4.0};
-    println!("{:#?}", pos1 + post2);
+    let pos2 = Vec3{x:2.0, y:3.0, z:4.0};
+    println!("{:#?}", pos1 + pos2);
+    println!("{:#?}", pos1 - pos2);
+    println!("{:#?}", pos1 * pos2);
+    println!("{:#?}", pos1 * 3.0);
+    println!("{:#?}", pos1 / 3.0);
     println!("Hello, world!");
 }
